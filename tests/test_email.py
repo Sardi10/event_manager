c0ddd9b94ff8ea068e882c611635d8d7985b1d50
Sardi10 <sardikuka@gmail.com>
2025-04-13 07:29:11 -0400

    Mocked email service parameters

--- tests/test_email.py
+++ tests/test_email.py
@@ -4,7 +4,13 @@ from app.utils.template_manager import TemplateManager
 
     
 @pytest.mark.asyncio
-async def test_send_markdown_email(email_service):
+async def test_send_markdown_email(mocker):
+    
+    mocker.patch('app.utils.smtp_connection.SMTPClient.send_email', return_value=None)
+    
+    template_manager = TemplateManager()
+    email_service = EmailService(template_manager=template_manager)
+
     user_data = {
         "email": "test@example.com",
         "name": "Test User",
